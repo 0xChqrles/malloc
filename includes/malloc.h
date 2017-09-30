0df4561dc840eb1dcd9db1fbda9db240b8c294b4
Charles LANIER <clanier@e1r12p6.42.fr>
2017-10-01 00:15:39 +0200

    restart malloc.c from the beginning

--- includes/malloc.h
+++ includes/malloc.h
@@ -6,7 +6,7 @@
 /*   By: clanier <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/09/23 16:27:40 by clanier           #+#    #+#             */
-/*   Updated: 2017/09/23 21:33:52 by clanier          ###   ########.fr       */
+/*   Updated: 2017/10/01 00:14:46 by clanier          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,8 @@
 
 # define TINY 2048
 # define SMALL 4096
+# define MMAP_PROT MMAP_READ MMAP_WRITE
+# define MMAP_FLAGS MMAP_ANON MMAP_PRIVATE
 
 # include <sys/mman.h>
 # include <fcntl.h>
@@ -22,7 +24,33 @@
 # include <sys/stat.h>
 # include <sys/types.h>
 # include <stdlib.h>
+# include <stdbool.h>
 
 void	*malloc(size_t size);
+
+typedef			s_line
+{
+	void		*tiny;
+	void		*small;
+	t_malloc	*large;
+	rlim_t		limit;
+}				t_line;
+
+typedef			s_malloc
+{
+	t_malloc	*next;
+	t_malloc	*prev;
+	bool		isFree;
+	size_t		size;
+}
+
+typedef			s_page
+{
+	void		*next;
+	void		*prev;
+	void		*end;
+	size_t		size;
+	t_malloc	*first;
+}				t_page;
 
 #endif
--- srcs/malloc.c
+++ srcs/malloc.c
@@ -6,7 +6,7 @@
 /*   By: clanier <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/09/23 15:36:00 by clanier           #+#    #+#             */
-/*   Updated: 2017/09/24 17:24:53 by clanier          ###   ########.fr       */
+/*   Updated: 2017/10/01 00:06:16 by clanier          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,55 +14,145 @@
 #include <stdio.h>
 #include <err.h>
 
-void	*allocMem(size_t size)
-{
-	void	*ptr;
+t_line	line = {NULL, NULL, NULL, 0};
 
-	if (size <= 0)
-		return (NULL);
-	ptr = mmap (0, size, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANON, -1, 0);
-	if (ptr == MAP_FAILED)
-		return (NULL);
-	return (ptr);
+void	*initMalloc(size_t size, void **page)
+{
+	t_malloc	*head;
+
+	head = (t_malloc*)(*page + size);
+	head->size = size - sizeof(t_page) - sizeof(t_malloc);
+	head->prev = NULL;
+	head->next = NULL;
+	head->isFree = true;
+	return (head);
 }
 
-int		getPageMultiple(size_t size)
+void	*initPage(size_t size, void **page, void *prev)
 {
-	size_t	multiple;
-	int		pageSize;
+	t_page	*head;
+
+	head = (t_map*)(*page);
+	head->first = initMalloc(size, *page);
+	head->prev = prev;
+	head->next = NULL;
+	head->size = size;
+	head->end = *page + size - 1;
+	if (prev)
+		prev->next = head;
+	return (head);
+}
+
+int		createMap(size_t allocMax, void	**page, void *prev)
+{
+	size_t	size;
+	size_t	pageSize;
 
 	pageSize = getpagesize();
-	multiple = 100;
-	while (multiple * size % pageSize)
-		multiple++;
-	return (multiple);
+	size = 100 * (allocMax + sizeof(t_malloc)) + sizeof(t_page);
+	if (size % pageSize)
+		size += pageSize - size % pageSize;
+	if (size > line.limit)
+		return (-1);
+	if ((*page = mmap(0, size, MMAP_PROT, MAP_FLAGS, -1, 0)) == MMAP_FAILED)
+		return (-1);
+	line.limit -= size;
+	initMap(size, page, prev);
 }
 
-void	*initZone(void **page, size_t pageSize, size_t size, int offset)
+int		init()
 {
-	if (!(*page) || sizeof(*page) - (offset - size) < size)
+	struct rlimit	limit;
+
+	if (!getrlimit(RLIMIT_MEMLOCK, &limit))
+		line.limit = limit.rlim_cur;
+	if (createPage(TINY, &line.tiny, NULL) < 0)
+		return (-1);
+	if (createPage(SMALL, &line.small, NULL) < 0)
+		return (-1);
+	return (0);
+	i =
+}
+
+void	*setMalloc(size_t size, t_malloc **mem)
+{
+	t_malloc	*new;
+
+	new = (void*)mem + sizeof(t_malloc) + size;
+	new->size = mem->size - size - sizeof(t_malloc);
+	new->isFree = true;
+	new->next = mem->next;
+	new->prev = *mem;
+	(*mem)->next = new;
+	(*mem)->isfree = false;
+	(*mem)->size = size;
+	return ((*mem) + sizeof(t_malloc));
+}
+
+void	*searchMemory(size_t size, void **page, size_t sizeMax)
+{
+	t_malloc	*mtmp;
+	t_page		*ptmp;
+
+	ptmp = (t_page*)(*page);
+	while (ptmp)
 	{
-		if (*page)
-			free(*page);
-		*page = allocMem(size * getPageMultiple(size));
+		mtmp = ptmp->first;
+		while (mtmp)
+		{
+			if (mtmp->isfree && mtmp->size > size + sizeof(t_malloc))
+				return (setMalloc(size, &mtmp));
+			else if (mtmp->isFree && mtmp->size >= size)
+			{
+				mtmp->isFree = false;
+				return ((void*)mtmp + sizeof(t_malloc));
+			}
+			mtmp = mtmp->next;
+		}
 	}
-	printf("%d\n", sizeof(*page));
-	printf("%p\n", *page);
-	return (*page + (offset - size));
-//	return (/* fonction qui va juste decouper et te renvoyer ce dont t'as besoin */);
+	if (createPage(SMALL, page, *page) < 0)
+		return (NULL);
+	ptmp = (t_page*)(*page);
+	mtmp = ptmp->first;
+	return (setMalloc(size, &mtmp));
+}
+
+void	*largeMalloc(size_t size)
+{
+	void		*ptr;
+	t_malloc	*tmp;
+	t_malloc	*new;
+
+	size += sizeof(t_malloc);
+	if (line.limit < size
+	|| (ptr = mmap(0, size, MMAP_PROT, MAP_FLAGS, -1, 0)) == MMAP_FAILED)
+		return (NULL);
+	line.limit -= size;
+	new = (*t_malloc)ptr;
+	new->next = NULL;
+	new->prev = NULL;
+	new->isFree = false;
+	new->isFree = size - sizeof(t_malloc);
+	if (!line.large)
+		line.large = new;
+	else
+	{
+		tmp = line.large;
+		while (tmp->next)
+			tmp = tmp->next;
+		tmp->next = new;
+		new->prev = tmp;
+	}
+	return (ptr + sizeof(t_malloc));
 }
 
 void	*malloc(size_t size)
 {
-	static int	tinyOffset = 0;
-	static int	smallOffset = 0;
-	static void	*tiny;
-	static void	*small;
-
-	if (size > SMALL)
-		return (allocMem(size));
-	if (size <= TINY)
-		return (initZone(&tiny, TINY, size, tinyOffset += size));
-	else
-		return (initZone(&small, SMALL, size, smallOffset += size));
+	if ((!line.tiny && init() < 0) || size <= 0)
+		return (NULL);
+	if (size < TINY)
+		return (searchMemory(size, &line.tiny, TINY));
+	if (size < SMALL)
+		return (searchMemory(size, &line.small, SMALL));
+	largeMalloc(size, &line.large);
 }
