d0384d22babe7307397af4597ad2b5dbdf81a425
Charles LANIER <clanier@e3r1p6.42.fr>
2017-12-04 18:32:33 +0100

    fixing realloc move_data bug

--- srcs/realloc.c
+++ srcs/realloc.c
@@ -6,7 +6,7 @@
 /*   By: clanier <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/10/01 20:55:13 by clanier           #+#    #+#             */
-/*   Updated: 2017/12/04 16:57:50 by clanier          ###   ########.fr       */
+/*   Updated: 2017/12/04 18:26:46 by clanier          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,8 +19,7 @@ void	*move_data(t_malloc *src, void *dest, size_t size)
 	i = 0;
 	while (i < size && i < src->size)
 	{
-		((unsigned char*)(dest))[i] =
-		((unsigned char*)(src + sizeof(t_malloc)))[i];
+		((char*)(dest))[i] = ((char*)((size_t)src + sizeof(t_malloc)))[i];
 		i++;
 	}
 	return (dest);
@@ -31,7 +30,7 @@ void	*realloc_memory(void *ptr, size_t size, char page_type)
 	t_malloc	*mtmp;
 	void		*new;
 
-	mtmp = (t_malloc*)ptr;
+	mtmp = ptr;
 	g_line.old_size = mtmp->size;
 	if (size <= mtmp->size
 	&& (page_type == 't' || (page_type == 's' && size > TINY)))
